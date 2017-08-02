cffc3dd294d268b328a4556070e43c21f2a1c1af
JoMingyu <city7310@naver.com>
2017-08-03 08:52:45 +0900

    True&False on Data

---
+++ 009. True&False on Data/main.py
@@ -0,0 +1,18 @@
+# 모든 자료형은 참과 거짓이 있다
+# 일단 당연히 True는 참, False는 거짓
+# 비어있는 리스트, 딕셔너리, 튜플, 문자열은 거짓
+# 숫자 0은 거짓, 1은 참
+# None은 거짓
+
+# if문은 조금 뒤에 배우지만 그러려니 하고 보도록 하자
+if 'hello':
+    print('hello')
+
+if 1:
+    print('number 1')
+
+if [1, 2]:
+    print('list')
+
+if None:
+    print('None')
