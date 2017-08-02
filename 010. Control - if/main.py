aeb1923c7757f7602d9a01b66f112fef5b6482e1
JoMingyu <city7310@naver.com>
2017-08-03 08:52:51 +0900

    Control - if

---
+++ 010. Control - if/main.py
@@ -0,0 +1,30 @@
+# 구조적으로 프로그래밍을 하기 위해 필요한 조건문
+# 1. if - elif - else 형태로 조건분기를 구성한다
+# 2. if 안에 소괄호를 씌우지 않는다
+# 3. 중괄호가 아니라 콜론과 indent로 블럭을 구분한다
+
+if True:
+    print('True')
+
+# 기본 비교 연산자는 다른 언어들이랑 똑같다(<, >, =<, => 등등)
+# 논리 연산자는 조금 다르다(and, or, not)
+
+if True or False:
+    print('True of False')
+
+if not False:
+    print('not False')
+
+if False and False:
+    print('False and False')
+
+# Iterable data 안에 특정 값이 들어 있는지 체크할 수가 있다(in/not in)
+lst = [1, 2, 3]
+if 4 in lst:
+    print('4 in list!')
+elif 3 not in lst:
+    print('3 not in list!')
+elif 1 in lst:
+    print('1 in list!')
+else:
+    print("I don't know")
