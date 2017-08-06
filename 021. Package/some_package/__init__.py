cf2b01ac5626115145c0d4bc6406637d3abe2000
JoMingyu <city7310@naver.com>
2017-08-07 00:11:07 +0900

    Package

---
+++ 021. Package/some_package/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['another_module', 'another_module2']
---
+++ 021. Package/some_package/another_module.py
@@ -0,0 +1,10 @@
+# 패키지를 생성하면 __init__.py가 생긴다. python 3.3부턴 이 파일 없이도 패키지로 인식이 되긴 한다
+#
+
+
+def say_hello():
+    print('Hello')
+
+
+def say_hi():
+    print('hi')
