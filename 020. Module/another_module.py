92ddd150434413724a2db55fc6b3fa41039e54d7
JoMingyu <city7310@naver.com>
2017-08-06 09:36:33 +0900

    Module

---
+++ 020. Module/another_module.py
@@ -0,0 +1,6 @@
+# another_module이라는 모듈에 함수 하나를 선언해 뒀다
+def say_hello():
+    print('Hello')
+
+def say_hi():
+    print('Hi')
---
+++ 020. Module/main.py
@@ -0,0 +1,16 @@
+# 파이썬은 모듈화가 참 중요하다. 코드 재사용을 위해서
+# 이것도 main 모듈이다
+
+# another_module을 불러오려면 import를 쓰면 된다
+import another_module
+another_module.say_hello()
+
+# from ~ import를 이용하면 특정 함수나 클래스만 직접 import할 수 있다
+from another_module import say_hello
+say_hello()
+
+# 와일드카드 import가 가능하다
+from another_module import *
+# another_module 내의 모든 것을 import한다
+say_hello()
+say_hi()
