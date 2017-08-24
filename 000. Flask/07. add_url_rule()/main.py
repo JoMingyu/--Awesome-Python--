a6e1fd151e3b427433734d97c33c2e23f819cbe6
PlanB <city7310@naver.com>
2017-08-24 09:42:27 +0900

    07. add_url_rule()

---
+++ 000. Flask/07. add_url_rule()/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# 데코레이터 없이 함수를 선언하고
+def index():
+    return 'hello'
+
+app.add_url_rule('/index', 'endpoint!!', index)
+# add_url_rule이라는 함수로 리소스를 추가한다
+# rule, endpoint, view_func 순서
+
+# route라는 메소드는 데코레이터 안에서 self.add_url_rule로 룰을 추가한다
+
+app.run()
