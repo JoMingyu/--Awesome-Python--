9c65f0f7571c8ce0447761dce4dc98eca8896aa7
PlanB <city7310@naver.com>
2017-08-25 11:43:27 +0900

    08. Access request parameter

---
+++ 000. Flask/08. Access request parameter/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+# 요청 데이터에 접근할 필요가 있다
+# 특히 GET 메소드로 정의한 리소스에서 사용한다
+# flask 모듈에서 request 객체를 가져와야 한다
+
+
+@app.route('/')
+def index():
+    return request.args['test_key']
+    # args라는 딕셔너리에 접근해서 빼오면 된다
+
+app.run(debug=True)
