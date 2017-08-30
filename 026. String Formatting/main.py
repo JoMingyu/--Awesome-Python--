2339a07dbf27f0993367a5a55343f6ca509640df
PlanB <city7310@naver.com>
2017-08-31 00:00:41 +0900

    String Formatting

--- 026. String Formatting/main.py
+++ 026. String Formatting/main.py
@@ -1,6 +1,6 @@
-# 포매팅 방식은 여러가지가 있다
-format1 = 'My name is %s'
-print(format1 % 'PlanB')
+﻿# 포매팅 방식은 여러가지가 있다
+format1 = 'My %s is %s!!'
+print(format1 % ('Name', 'PlanB'))
 
 # str 객체엔 format이라는 메소드가 있다
 # 문자열로 포맷을 만들고, format 메소드의 인자로 값들을 넘겨주면 된다
