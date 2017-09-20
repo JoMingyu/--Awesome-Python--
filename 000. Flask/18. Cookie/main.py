178522dc6c3777447db4425589d5f92366cddbca
PlanB <city7310@naver.com>
2017-09-20 12:03:06 +0900

    18. Cookie

--- 000. Flask/18. Cookie/main.py
+++ 000. Flask/18. Cookie/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request
+﻿from flask import Flask, make_response, request
 # 모두 알다시피 HTTP는 connectionless와 stateless라는 특징을 가진다
 # 한번의 요청-응답이 이루어지고 나면 접속을 끊으며, 통신이 끝나면 상태 정보를 유지하지 않는다
 # 그래서 쿠키와 세션을 사용한다
@@ -22,6 +22,10 @@ def verify_cookie():
     # 파라미터와 body를 받았을 때처럼 얘도 딕셔너리
     print(request.cookies['cookie'])
 
-    return ''
+    # 쿠키 제거는
+    response = make_response('hello hello!')
+    response.set_cookie('cookie', '', expires=0)
+
+    return response
 
 app.run(debug=True)
