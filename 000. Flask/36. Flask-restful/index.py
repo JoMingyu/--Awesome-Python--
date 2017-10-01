2942a593c00b36c1e1b124e5497a4c2058d94dc6
JoMingyu <city7310@naver.com>
2017-10-02 00:52:20 +0900

    36. Flask-restful

---
+++ 000. Flask/36. Flask-restful/index.py
@@ -0,0 +1,15 @@
+﻿from flask_restful import Resource, abort
+# flask_restful의 Resource 클래스를 이용한다
+# Resource 클래스를 상속받은 클래스는 하나의 uri에 대한 자원(resource)이 된다
+# Flask에서 사용하던 여러가지 리소스들은 flask-restful에서도 제공된다
+
+
+class Index(Resource):
+    # app.route() 방식으로 라우트했던 게 메소드화 되었다
+    def get(self):
+        return [1, 2, 3]
+        # Flask의 route 데코레이터를 통한 라우팅에서 이 구문은 TypeError를 발생시키지만
+        # flask-restful은 이를 알아서 처리해주는 듯 하다
+
+    def post(self):
+        abort(400)
---
+++ 000. Flask/36. Flask-restful/main.py
@@ -0,0 +1,20 @@
+# pip install flask-restful
+# Flask 공식 문서에선 별도로 언급하지 않는 좋은 모듈
+# RESTful API를 찍어내는 데에 도움을 준다
+from flask import Flask, Blueprint
+from flask_restful import Api
+
+blueprint = Blueprint('sample_blueprint', __name__)
+api = Api(blueprint)
+# Api 클래스의 생성자에 Flask 인스턴스를 전달하며 api 객체를 얻어냄
+
+from index import Index
+api.add_resource(Index, '/')
+# api 객체의 add_resource 메소드를 이용해 리소스와 uri를 매핑
+
+app = Flask(__name__)
+app.register_blueprint(blueprint)
+# 블루프린트 등록
+
+if __name__ == '__main__':
+    app.run(debug=True)
