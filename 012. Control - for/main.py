41181f4d0251997b33244187a48b114444cdc686
Sangmin <sm372181@gmail.com>
2017-10-04 22:05:42 +0900

    Supplement the contents.

--- 012. Control - for/main.py
+++ 012. Control - for/main.py
@@ -4,11 +4,17 @@
 lst = [1, 2, 3, 4]
 for i in lst:
     print(i)
+# 실행 과정
+# 1. lst의 첫 번째 원소인 1이 i에 대입되어 for문 블록 내에 있는 작업을 수행한다
+# 2. lst의 두 번째 원소인 2가 i에 대입되어 for문 블록 내에 있는 작업을 수행한다
+# 3. lst의 세 번재 원소인 3이 i에 대입되어 for문 블록 내에 있는 작업을 수행한다
+# 4. lst의 네 번째 원소인 4가 i에 대입되어 for분 블록 내에 있는 작업을 수행한다
+# 5. lst내에 있는 모든 원소에 대해 작업이 끝났기에 for문을 끝낸다
 
 # 빌트인 함수 중 range라는 게 있다
 for i in range(0, 5):
     print(i)
-# range(n, m) -> n부터 m-1
+# range(n, m) -> n부터 m-1 까지의 iterator를 반환해준다
 
 for i in range(5):
     print(i)
