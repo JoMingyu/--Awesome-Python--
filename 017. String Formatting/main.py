7b2c8782d765097796c50bc541b38f0b7cd20fdf
PlanB <city7310@naver.com>
2017-10-05 14:25:32 +0900

    -Renew- String Formatting, User IO, Type Casting

---
+++ 017. String Formatting/main.py
@@ -0,0 +1,14 @@
+﻿# 포매팅 방식은 여러가지가 있다
+format1 = 'My %s is %s!!'
+print(format1 % ('Name', 'PlanB'))
+
+# str 객체엔 format이라는 메소드가 있다
+# 문자열로 포맷을 만들고, format 메소드의 인자로 값들을 넘겨주면 된다
+format2 = "This is {0} formatting!"
+print(format2.format('awesome'))
+# 타입 안정성이 높고, 정렬이나 공백 채우기 등의 고급 포매팅이 가능해서 이걸 많이 쓴다
+# https://wikidocs.net/13
+
+# 파이썬 3.6의 fstring
+data = 'formatting'
+print(f"Python 3.6's {data}")
---
+++ 018. User IO/main.py
@@ -0,0 +1,10 @@
+# Input에 대한 처리도 해보자
+user_input = input()
+print(user_input)
+
+# input 함수 안에 문자열을 집어넣어서 hint(=prompt)를 제공할 수 있다
+user_input = input('입력 > ')
+print(user_input)
+
+# input 함수의 타입은 무조건 str
+# 타입 캐스팅이 필요하다. 19에서 알아보자
