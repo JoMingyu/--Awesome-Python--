f4c486c7f34f597181861756507938d4300aadad
PlanB <city7310@naver.com>
2017-10-05 14:19:50 +0900

    -Renew- Control - List Comprehension

--- 013. Control - List Comprehension/main.py
+++ 013. Control - List Comprehension/main.py
@@ -1,19 +1,18 @@
-﻿# 리스트를 [2, 4, 6, 8] 이런 식으로 만들어야 될 때도 있을 것이다
+﻿# 리스트를 [2, 4, 6, 8] 과 같은 식으로 만들어야 될 때도 있을 것이다
 # 리스트를 만드는 부분 자체를 for문으로 대체할 수 있다
 # List Comprehension이라고 부른다
 lst = [i for i in range(10)]
 print(lst)
 
-# 좀 복잡하게도 된다
+# 연산자를 넣어 보자
 lst = [i * 2 for i in range(1, 11)]
-# 1부터 10까지의 range에 2를 곱합
 print(lst)
 
 # if문도 적용된다
 lst = [i for i in range(100) if i % 3 == 0]
 print(lst)
 
-# for문을 여러개 사용할 수도 있다
+# for문을 중첩할 수도 있다
 lst = [i * j for i in range(2, 10)
        for j in range(1, 10)]
 print(lst)
