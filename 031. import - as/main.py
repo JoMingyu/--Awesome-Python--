3bbe3de9f4a78782fcbcd7da3d3200911c302af3
JoMingyu <city7310@naver.com>
2017-10-09 00:18:39 +0900

    -Renew- import-as

---
+++ 031. import - as/main.py
@@ -0,0 +1,9 @@
+from another_module import data as d
+from another_module import func as f
+# 특정 모듈에서 값을 가져올 때(변수나 함수를 가져올 때)
+# import [data] as [var] 형식으로 현재 모듈에 한해 네이밍을 바꿔줄 수 있다
+# import 이름이 겹치는 등의 문제를 해결할 수 있다
+
+
+print(d)
+f('qweqwe')
