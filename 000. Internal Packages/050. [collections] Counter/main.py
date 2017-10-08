abffd6103605e32e6e018241d5b3d384cbc96d5d
JoMingyu <city7310@naver.com>
2017-10-08 22:06:49 +0900

    Remove legacy internal packages TIL

--- 000. Internal Packages/050. [collections] Counter/main.py
+++
@@ -1,9 +0,0 @@
-import collections
-
-_list = [1, 2, 3, 3, 2, 4, 5, 3, 3]
-# 리스트 내의 요소가 몇 번이나 반복되었는지 카운트할 경우가 생긴다
-
-counter = collections.Counter(_list)
-print(counter)
-print(counter[3])
-print(counter.most_common(2))
