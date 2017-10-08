bb728f17f9c3d82ad49e2c36fe16dcc1fef8ca65
JoMingyu <city7310@naver.com>
2017-10-08 22:05:57 +0900

    Remove legacy external packages TIL

--- 000. External Packages/037. [requests] HTTP client/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-# 간단한 HTTP 요청을 위한 모듈
-# pip install requests
-
-# request 함수가 있으나 메소드 명시하는 게 좋다
-response = requests.get('http://naver.com')
-
-# response 객체가 있다. 여기서 여러가지 데이터들을 뜯어올 수 있다
-print(response.content)
--- 000. External Packages/040. [pyfcm] data_message/main.py
+++
@@ -1,7 +0,0 @@
-from pyfcm import FCMNotification
-# fcm 푸쉬 메시지는 data message를 전송할 수 있다
-
-fcm = FCMNotification(api_key='')
-
-data = {'key1': 123, 'key2': 1234}
-fcm.notify_single_device(data_message=data)
