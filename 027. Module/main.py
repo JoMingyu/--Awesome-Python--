5a033ae72afd5ec17b49ea50ca0901146393fe8d
PlanB <city7310@naver.com>
2017-10-10 13:21:20 +0900

    -Renew- Module

--- 027. Module/main.py
+++ 027. Module/main.py
@@ -1,4 +1,4 @@
-# 코드 재사용을 위해서 모듈화는 참 중요하다
+﻿# 코드 재사용을 위해서 모듈화는 참 중요하다
 # 여기도 main 모듈이다
 
 # another_module을 불러오려면 import를 쓰면 된다
@@ -14,3 +14,9 @@ from another_module import *
 # another_module 내의 모든 것을 import한다
 say_hello()
 say_hi()
+
+# PEP8 표준의 import
+# 항상 파일의 맨 위에 import문을 놓는다
+# 모듈을 import할 때는 항상 모듈의 절대 이름을 사용하며 현재 모듈을 기준으로 한 상대 경로를 사용하지 않는다
+# 상대적인 import를 해야 한다면 명시적인 구문을 써서 from . import another_module이라고 한다
+# import는 표준 라이브러리 모듈, 서드파티 모듈, 자신이 만든 모듈 순으로 구분해야 한다
