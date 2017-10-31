05c473fac7f72fb1de7102339989e2248c3a1da2
PlanB <city7310@naver.com>
2017-10-31 13:20:48 +0900

    pkgutil

---
+++ 000. Internal Packages/014. [pkgutil] Reflection/main.py
@@ -0,0 +1,15 @@
+import pkgutil
+# pkgutil을 활용해 프로젝트의 패키지와 모듈들을 동적으로 다룰 수 있다
+import test
+
+
+for loader, name, is_package in pkgutil.iter_modules(test.__path__):
+    # FileFinder 객체, 모듈 이름, 패키지 여부 순
+    print(loader, name, is_package)
+
+    module_ = loader.find_module(name).load_module(name)
+    # 모듈 동적 load
+    print(module_)
+
+# 재귀함수를 구성하고, 특정 패키지 하위의 모든 모듈들을 불러오는 식의 리플렉션을 구현할 수 있을 것이다
+# https://github.com/JoMingyu/Reflections
