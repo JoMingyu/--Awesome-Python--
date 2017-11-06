a26f71fc993665925a4eaa81916f1a5b92e1160c
PlanB <city7310@naver.com>
2017-11-06 13:27:19 +0900

    pyfcm 1

--- 000. External Packages/002-1. [pyfcm] Firebase cloud messaging/main.py
+++ 000. External Packages/002-1. [pyfcm] Firebase cloud messaging/main.py
@@ -11,3 +11,6 @@ fcm.notify_single_device(registration_id='', message_title='제목이야', messa
 
 fcm.notify_multiple_devices(registration_ids=['', ''], message_title='제-목', message_body='배고파')
 # 여러 클라이언트에 보내는 메소드. registration_ids 파라미터로 리스트 형태의 클라이언트 키들을 전달한다
+
+fcm.notify_topic_subscribers(topic_name='', message_title='제-목', message_body='body!')
+# 특정 topic을 subscribe하고 있는 디바이스에 보내기
