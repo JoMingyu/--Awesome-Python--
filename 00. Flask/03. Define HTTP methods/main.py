40b2718237602e0213eab7b24c49a3270294daf7
PlanB <city7310@naver.com>
2017-11-20 10:52:21 +0900

    Define HTTP methods

--- 00. Flask/03. Define HTTP methods/main.py
+++ 00. Flask/03. Define HTTP methods/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+﻿from flask import Flask
 
 app = Flask(__name__)
 # 평범하게 객체를 생성하고
@@ -19,5 +19,6 @@ def test():
 # methods는 위에서 보듯이 리스트로 전달하기 때문에 여러 HTTP 메소드를 지정할 수 있다
 # 표준화된 HTTP 메소드가 지정되지 않더라도 오류는 뜨지 않는다
 # 대/소문자에 영향을 받지 않는다. pOsT라고 써도 잘 된다
+# ** methods 파라미터 명시는 생략할 수 있다
 
 app.run(debug=True)
