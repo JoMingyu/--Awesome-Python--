73449bd38544ce06bf92dadd9c7a21e82e1e8e6c
PlanB <city7310@naver.com>
2017-11-25 00:51:11 +0900

    request.args.get & form.get, Handling unicode in response

--- 00. Flask/13. request.args.get & form.get/main.py
+++ 00. Flask/13. request.args.get & form.get/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+﻿from flask import Flask, request
 
 app = Flask(__name__)
 
--- 00. Flask/17. Handling unicode in response/main.py
+++ 00. Flask/17. Handling unicode in response/main.py
@@ -1,4 +1,4 @@
-# Flask는 텍스트를 처리할 때 유니코드를 사용한다. 유니코드 처리를 클라이언트 측에서 해준다면 좋겠지만, 그런 일은 자주 없다
+﻿# Flask는 텍스트를 처리할 때 유니코드를 사용한다. 유니코드 처리를 클라이언트 측에서 해준다면 좋겠지만, 그런 일은 자주 없다
 # flask에 있는 Response 클래스와 json 모듈의 도움을 받아 해결한다
 import json
 
@@ -15,11 +15,11 @@ def unicode():
 
 @app.route('/encoded')
 def encoded():
-    return Response(json.dumps(resp, ensure_ascii=False))
+    return Response(json.dumps(resp, ensure_ascii=False), content_type='application/json; charset=utf8')
 
 # /unicode, /encoded 모두 웹브라우저로 접속해서 결과물을 보도록 하자
 # /unicode의 경우 한글 부분이 유니코드로 처리되고
 # /encoded의 경우 한글이 제대로 보인다
-# jsonify와 다르게 헤더(application/json)를 설정해주지 않는다는 것을 주의해야 한다
+# 아래의 경우에는 jsonify와 다르게 헤더(application/json)를 따로 설정해야 한다는 것을 주의해야 한다
 
 app.run(debug=True)
