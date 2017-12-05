9e8213c7f20153998dea9af6dd0af7e2d0cef33a
PlanB <city7310@naver.com>
2017-12-06 08:49:33 +0900

    Flask-restful-swagger-2

--- 00. Flask/31. Flask-restful-swagger-2/main.py
+++ 00. Flask/31. Flask-restful-swagger-2/main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+﻿from flask import Flask
 from flask_cors import CORS
 from flask_restful_swagger_2 import swagger, Api, Resource, request
-# Swagger라는 멋진 API 관리 툴이 있는데, flask-restful-swagger 패키지를 이용해서 자동화 가능하다
-# from flask_restful import Api 대신 위의 import 방식을 사용하자
+# pip install flask-restful-swagger-2
+# Swagger라는 멋진 API 관리 툴이 있는데, flask-restful-swagger 패키지를 이용해서 Flask-restful 기반으로 문서화 가능하다
+# 실제로 라이브러리 내부에서는 Swagger에 관련된 작업 후 flask-restful로 라우팅 처리를 넘긴다
 
 app = Flask(__name__)
 CORS(app)
-api = Api(app, api_version='1.0')
+# Swagger에는 CORS 지원이 필요
+
+api = Api(app, api_version='1.0', title='Sample Server', description='This is Sample API Server!')
+# Swagger Docs에 보여질 API 버전, 타이틀, description
+# api_spec_url 파라미터도 있음(기본값은 /api/swagger)
+# 부가적인 것들은 Api 클래스를 참조하도록 하자
 
 
 class Sample(Resource):
@@ -30,8 +36,11 @@ class Sample(Resource):
                 # 파라미터가 들어갈 곳
                 # must be path, query, header, body or formData
 
-                'type': 'int'
+                'type': 'int',
                 # 타입
+
+                'required': True
+                # 필수 여부
             },
             {
                 'name': 'test_form',
@@ -62,9 +71,11 @@ class Sample(Resource):
         return {
             'id': id,
             'form_data': request.form.get('test_form')
-        }
+        }, 200
 
 api.add_resource(Sample, '/sample/<id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# http://petstore.swagger.io/?url=http://localhost:5000/api/swagger.json
