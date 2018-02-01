20e3691c46bebb37f76bf4e57333da9c9f5601c8
JoMingyu <city7310@naver.com>
2018-02-01 14:42:15 +0900

    031 ~ 032

--- 031. with - as/main.py
+++ 031. with - as/main.py
@@ -1,9 +1,11 @@
 ﻿# with-as 문은 파이썬 2.5에서 도입된 기능
 # 객체 사용 전과 후에 꼭 해줘야 하는 것들을 줄이는 일을 with-as 문으로 해결할 수 있다
-# 객체에는 인터프리터의 context manager에 의해서 실행되는 __enter__()와 __exit__() 메소드가 정의되어 있어야 한다.
+# 객체에는 인터프리터의 context manager에 의해서 실행되는 __enter__()와 __exit__() 메소드가 정의되어 있어야 with-as문을 적용할 수 있다
 
-# 파이썬의 file 객체는 __enter__와 __exit__ 함수가 구현되어 있다. 각자 file object 객체 자신을 리턴하고, 후자는 file을 닫아준다.
+# 파이썬의 file 객체는 __enter__와 __exit__ 함수가 구현되어 있다. 각각 file object 객체 자신을 리턴하고, 후자는 file을 닫아준다
 with open('test.txt', 'w') as f:
-    # __enter__()의 리턴 값이 f로
+    # open()시 리턴되는 파일 객체가 with을 만나 __enter__()를 호출
+    # 파일 객체의 __enter__() 메소드의 리턴 값이 f로 사용됨. 여기서는 파일 객체 자신
     f.write('hello')
-    # 블럭이 끝날 때 __exit__() 호출
+    # with 블럭이 종료되며 __exit__() 호출
+    # 여기서는 파일 객체를 닫아줌(f.close())
--- 032. import - as/main.py
+++ 032. import - as/main.py
@@ -1,9 +1,8 @@
 from another_module import data as d
 from another_module import func as f
-# 특정 모듈에서 값을 가져올 때(변수나 함수를 가져올 때)
-# import [data] as [var] 형식으로 현재 모듈에 한해 네이밍을 바꿔줄 수 있다
-# import 이름이 겹치는 등의 문제를 해결할 수 있다
-
+# import 당시에
+# import [name] as [alias] 형식으로 현재 모듈에 한해 별명(alias)을 만들어줄 수 있다
+# import 이름이 겹치거나, 이름이 너무 길거나, 마음에 안드는 등의 문제를 해결할 수 있다
 
 print(d)
 f('qweqwe')
