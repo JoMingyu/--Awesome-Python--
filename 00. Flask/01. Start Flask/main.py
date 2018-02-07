702d3fd2f8d284aee7cc255b45c53ff16b1f7f73
JoMingyu <city7310@naver.com>
2018-02-07 09:39:15 +0900

    Flask - 01

--- 00. Flask/01. Start Flask/main.py
+++ 00. Flask/01. Start Flask/main.py
@@ -7,21 +7,24 @@ from flask import Flask
 
 app = Flask(__name__)
 # 1. 현재 모듈에 대해 Flask 객체를 생성
-
+# flask.app.Flask - https://github.com/pallets/flask/blob/master/flask/app.py#L67
 
 @app.route('/')
 # 2. 데코레이터를 통해 '/'에 라우팅
+# flask.app.Flask.route() - https://github.com/pallets/flask/blob/master/flask/app.py#L1186
+# 'A decorator that is used to register a view function for a given URL rule'
 # 한 endpoint에 대해 할당된 함수를 view function이라고 부른다
 def index():
     return 'hello'
     # 문자열 'hello'를 응답으로 전달. status code는 자동 200으로 처리
 
 
-@app.route('/custom/<param>')
-# 사용자 정의 URL
+@app.route('/custom/<int:param>')
+# 사용자 정의 URL, 타입은 생략할 수 있으며 기본 타입은 str
 def custom(param):
     return param
 
 # 3. 서버 run
+# flask.app.Flask.run() - https://github.com/pallets/flask/blob/master/flask/app.py#L822
 app.run()
 # localhost:5000에서 동작한다
