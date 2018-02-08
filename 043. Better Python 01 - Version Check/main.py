e9f1184556c72526a4caabe6bf788d9e53cb780f
JoMingyu <city7310@naver.com>
2018-02-08 17:03:32 +0900

    043

--- 043. Better Python 01 - Version Check/main.py
+++ 043. Better Python 01 - Version Check/main.py
@@ -1,6 +1,6 @@
 # 파이썬에는 2개의 메이저 버전이 있다(Python 2, Python 3)
 # 어떤 파이썬 버전을 사용하고 있는지를 아는 것은 정말 중요하다
-# Python 2를 지원하는 패키지, Python 3를 지원하는 패키지가 다르고(이전의 Pandas)
+# Python 2를 지원하는 패키지, Python 3를 지원하는 패키지가 다르고(이전의 Pandas나 MySQL-Python 등)
 # 문법적인 차이(String formatting 등), 내부적인 차이(Python 2의 xrange, Python 3의 range)들도 존재하기 때문
 import sys
 
