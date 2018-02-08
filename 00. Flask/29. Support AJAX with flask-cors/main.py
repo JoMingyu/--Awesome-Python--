599713154eeb4e49cecd61f88481fdb60317bdc1
JoMingyu <city7310@naver.com>
2018-02-08 09:58:35 +0900

    Flask - 29

--- 00. Flask/29. Support AJAX with flask-cors/main.py
+++ 00. Flask/29. Support AJAX with flask-cors/main.py
@@ -1,8 +1,11 @@
 ﻿from flask import Flask
-from flask_cors import CORS
 # pip install flask-cors
-# AJAX를 지원하기 위해 flask-cors를 사용하면 된다
-# app 객체의 데코레이터를 활용해도 되는데, 이 패키지를 사용하면 한 줄로 CORS 헤더 세팅이 가능해서 편하다
+
+from flask_cors import CORS
+# Cross-Origin Resource Sharing(CORS)을 지원하기 위한 패키지
+# app 객체의 데코레이터(after_request)를 활용해도 되는데, 이 패키지를 사용하면 한 줄로 CORS 헤더 세팅이 가능해서 편하다
+# Flask-CORS도 내부적으로는 app 객체의 after_request 데코레이터를 이용한다
+# https://github.com/corydolphin/flask-cors/blob/master/flask_cors/extension.py#L153
 
 app = Flask(__name__)
 CORS(app)
