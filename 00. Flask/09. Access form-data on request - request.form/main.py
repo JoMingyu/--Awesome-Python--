f79a1b076b9481897202d599e2bb0a74fd677491
JoMingyu <city7310@naver.com>
2018-02-08 09:57:44 +0900

    Flask - 09

--- 00. Flask/09. Access form-data on request - request.form/main.py
+++ 00. Flask/09. Access form-data on request - request.form/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-# POST에서 자주 사용하는 multipart/form-data와 application/x-www-form-urlencoded에 접근하자
+# HTTP의 POST 메소드는 데이터를 서버로 보내는 방법 중 하나. request body의 타입은 Content-Type 헤더에 따라 결정됨
+# request.form은 application/x-www-form-urlencoded와 application/form-data 타입에 대응된다
 # 옆동네 Node.js의 경우 각각 multer, bodyParser를 사용해야 하지만 파이썬은 request 객체면 다 끝난다
 
 
