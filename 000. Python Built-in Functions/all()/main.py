dc5471bd7360ce44eefdfcbb87da900dcac1ec89
FlouieInCl <jerion7474@gmail.com>
2018-02-19 12:16:50 +0900

    Built-in Function all() and any()

---
+++ 000. Python Built-in Functions/all()/main.py
@@ -0,0 +1,26 @@
+"""
+Return True if bool(x) is True for all values x in the iterable.
+
+If the iterable is empty, return True.
+"""
+
+# all 함수는 파라미터로 반복가능한(iterable) 객체를 받는다
+# 이터레이블 객체의 모든 요소가 참이면 True 를 반환하고, 거짓이 하나라도 있으면 False 를 반환한다
+
+# 모두 True(참) 이므로 True
+print(all([True, True, True]))
+
+# 요소 중 0(거짓) 이 있으므로 False
+print(all([1, 2, 3, 0, 4]))
+
+# 비어있는 이터레이블 객체의 경우에도 True 를 반환한다
+print(all([]))
+
+
+# all 함수 원본
+# for 루프를 돌면서 없는 요소가 하나라도 있는지(거짓인지) 조건 검사를 한다
+def all(iterable):
+    for element in iterable:
+        if not element:
+            return False
+    return True
---
+++ 000. Python Built-in Functions/any()/main.py
@@ -0,0 +1,27 @@
+"""
+   Return True if bool(x) is True for any x in the iterable.
+
+   If the iterable is empty, return False.
+"""
+
+# any 함수는 all 함수와 마찬가지로 파라미터로 반복가능한(iterable) 객체를 받는다.
+# all 함수의 반대개념으로 생각하면 된다.
+# 이터레이블 객체의 요소들 중, 하나라도 참이면 True 를 반환한다.
+
+# 맨 마지막 요소 하나가 참이므로 True
+print(any([False, False, False, True]))
+
+# 모두 거짓이면 False
+print(any([0, 0, 0, 0]))
+
+# any 함수는 all 함수와 다르게 비어있는 이터레이블 객체가 파라미터로 들어오면 False 를 반환한다.
+print(any([]))
+
+
+# any 함수의 원본
+# all 함수의 코드와는 반대로 for 루프를 돌면서 요소 중, 참이 있다면 바로 True 를 반환하고, 아무 요소도 조건문에 걸리지 않으면 False 를 반환한다,
+def any(iterable):
+    for element in iterable:
+        if element:
+            return True
+    return False
