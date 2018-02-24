01d71404eec729e00d882f4f1d208073415a76a9
JoMingyu <city7310@naver.com>
2018-02-24 16:25:03 +0900

    Python Standard Library - 01-1

--- 000. Python Standard Library/01-1. [datetime] Date and Time/main.py
+++ 000. Python Standard Library/01-1. [datetime] Date and Time/main.py
@@ -1,6 +1,6 @@
 ﻿# https://docs.python.org/3/library/datetime.html
 # 시간을 다루는 datetime 모듈
-# datetime, date, time 클래스가 있다
+# datetime, date, time, timedelta, tzinfo, timezone 클래스가 있다
 import datetime
 
 date = datetime.date(2017, 8, 7)
@@ -36,3 +36,15 @@ print(now.date())
 
 print(now.time())
 # 시간만 가져오기
+
+# - 문자열과의 상호 변환
+# datetime/date/time 객체의 strftime() 메소드로 time -> str, datetime.datetime.strptime() 함수로 str -> time 변환할 수 있다
+
+# (1) datetime to str - strftime()
+print(now.strftime('%Y-%m-%d %H'))
+# datetime 모듈 하위 객체들은 __str__ 연산자를 오버로딩하고 있으며, 내부는 %Y-%m-%d %H:%M:%S.%f 포맷으로 변환하여 리턴한다
+print(str(now))
+
+# (2) str to datetime - strptime()
+print(datetime.datetime.strptime('2017-01-03', '%Y-%m-%d'))
+# 기타 포매팅은 여기서 확인하자 : https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
