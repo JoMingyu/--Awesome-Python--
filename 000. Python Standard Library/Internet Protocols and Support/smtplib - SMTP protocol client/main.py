e05622ba45a92fffab28f4e0467bc074ef011183
JoMingyu <city7310@naver.com>
2018-03-10 15:03:02 +0900

    smtplib

---
+++ 000. Python Standard Library/Internet Protocols and Support/smtplib - SMTP protocol client/main.py
@@ -0,0 +1,28 @@
+﻿# https://docs.python.org/3/library/smtplib.html
+# smtplib 모듈은 SMTP나 ESMTP listener daemon에게 메일 전송이 가능한 SMTP 클라이언트 세션 객체이다
+# RFC 821(Simple Mail Transfer Protocol), RFC 1869(SMTP Service Extensions) 기반으로 설계되어 있다
+from smtplib import SMTP
+from email.mime.text import MIMEText
+
+src_email = 'city7312@naver.com'
+dst_email = 'city7310@naver.com'
+
+smtp = SMTP('smtp.naver.com', 587)
+# SMTP 클래스의 인스턴스
+
+smtp.starttls()
+# tls가 필요한 경우
+
+smtp.login('city7312', 'password')
+# 로그인
+
+message = MIMEText('내용내용', _charset='utf-8')
+message['subject'] = '제목'
+message['from'] = src_email
+message['to'] = dst_email
+# MIMEText 클래스를 이용해 제목, 송신자, 수신자를 설정
+
+smtp.sendmail(src_email, dst_email, message.as_string())
+smtp.quit()
+
+# 추가적으로, SMTP 인스턴스는 __enter__()와 __exit__()이 구현되어 있기 때문에 with-as 문을 사용하는 것도 좋다
