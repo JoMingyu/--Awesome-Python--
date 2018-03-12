909573e46049ca61db91662c09249002a60c2d0d
JoMingyu <city7310@naver.com>
2018-03-13 08:39:45 +0900

    builtins

---
+++ 000. Python Standard Library/Python Runtime Services/builtins - Built-in objects/main.py
@@ -0,0 +1,16 @@
+# https://docs.python.org/3/library/builtins.html
+# 파이썬의 모든 빌트인에 직접 접근할 수 있다
+# 예를 들어, builtins.open()은 빌트인 함수 open()의 full name이다
+import builtins
+
+# 일반적으로 builtins.*** 형태로 명시적으로 액세스하지 않지만 빌트인과 같은 이름의 wrapper 함수를 만드는 등의 일을 할 때 유용하다
+class UpperCaser:
+    def __init__(self, f):
+        self._f = f
+    
+    def read(self, count=-1):
+        return self._f.read(count).upper()
+
+def open(path):
+    f = builtins.open(path, 'r')
+    return UpperCaser(f)
