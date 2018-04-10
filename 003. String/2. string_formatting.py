fb54fb781403f7259c7c6e83e1ea50b4bbabb275
JoMingyu <city7310@naver.com>
2018-04-10 19:33:43 +0900

    003 - 2

--- 003. String/2. string_formatting.py
+++ 003. String/2. string_formatting.py
@@ -1,5 +1,5 @@
-﻿# 포매팅 방식은 여러가지가 있다
-# - 보편적인(old-style) 문자열 포매팅 방식
+﻿# 문자열 포매팅은 문자열 안에 특정 값을 삽입하는 것을 말함
+# - 보편적인(old-style) 문자열 포매팅 방식, 포맷 코드를 사용
 format1 = 'My %s is %s!!'
 print(format1 % ('Name', 'PlanB'))
 
@@ -7,6 +7,7 @@ print(format1 % ('Name', 'PlanB'))
 # placeholder를 포함한 문자열로 포맷을 만들고, format 메소드의 인자로 값들을 넘겨주면 된다
 format2 = 'This is {0} formatting{1}'
 print(format2.format('awesome', '!'))
+# {0} 자리에 'awesome'이, {1} 자리에 '!'가 들어감
 # 타입 안정성이 높고, 정렬이나 공백 채우기 등의 고급 포매팅이 가능해서 이걸 많이 쓴다
 # https://pyformat.info/ 에 따르면 placeholder에 인덱스를 표기하는 위의 방식 또한 old-style로 분류한다
 
