f9860aad424f8da9a1786e43e792cf5046da025b
JoMingyu <city7310@naver.com>
2018-04-28 23:47:51 +0900

    Flask - Useful External Packages - 02

--- 000. Flask/00. Useful External Packages/02. Flask-restful/index.py
+++ 000. Flask/00. Useful External Packages/02. Flask-restful/index.py
@@ -1,7 +1,9 @@
-﻿from flask_restful import Resource, abort
+﻿from flask import abort
+from flask_restful import Resource
 # flask_restful의 Resource 클래스를 이용한다
+# flask.restful.Resource는 사실 Flask에서 플러거블 뷰의 메소드 기반 디스패치를 지원하기 위한 flask.views.MethodView 클래스를 상속받고 있다
 # Resource 클래스를 상속받은 클래스는 하나의 uri에 대한 자원(resource)이 된다
-# Flask에서 사용하던 여러가지 리소스들은 flask-restful에서도 제공된다
+# abort, request와 같이 flask에서 지원하는 필드들을 flask-restful에서도 지원하지만 그냥 flask에서 가져오는 게 좋다
 
 
 class Index(Resource):
