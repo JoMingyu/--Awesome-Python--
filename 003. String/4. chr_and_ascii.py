d2d3881a6e155a7284dd064ed6b5523342da8006
JoMingyu <city7310@naver.com>
2018-04-29 19:10:53 +0900

    Python basic 전체 제거

--- 003. String/4. chr_and_ascii.py
+++
@@ -1,6 +0,0 @@
-# 문자열과 ascii 값의 상호 변환은 각각 ord, chr을 사용한다
-print(ord('b'))
-# ascii 값으로 98
-
-print(chr(65))
-# 'A'
--- 012. Pass Statement/main.py
+++
@@ -1,5 +0,0 @@
-# 문법적으로 statement가 하나 이상 필요하지만, 아무 것도 하지 않으려 한다면 pass statement를 사용한다
-# statement가 들어갈 수 있는 요소(for, while, if, def 등)라면 모두 pass statement를 사용할 수 있다
-while True:
-    pass
-    # 키보드 인터럽트를 위한 busy-waiting
