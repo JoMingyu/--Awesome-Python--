5451e105c23f4550dfeb8246b7854ce5ffa0cb38
JoMingyu <city7310@naver.com>
2018-04-29 19:10:04 +0900

    New - 015

---
+++ New/015. If & Elif & Else Statement/main.py
@@ -0,0 +1,17 @@
+# if statemant는 Python에서 조건문을 작성하기 위한 키워드로
+# 조건을 판단하여 해당 조건에 맞는 작업을 수행하는 데 쓰인다
+# if [condition] 형태로 작성한다. condition 안에 별도의 괄호를 씌우지 않는다
+if True:
+    # Python은 대부분의 언어들에서 사용하는 중괄호 대신 콜론(:)과 들여쓰기를 사용
+    print('Condition is True!')
+
+if []:
+    print('List [] is True!')
+elif ():
+    print('Tuple () is True!')
+elif {}:
+    print('Dictionary {} is True!')
+else:
+    print('All is False..')
+
+# if - elif - else 형태로 조건분기를 구성한다
