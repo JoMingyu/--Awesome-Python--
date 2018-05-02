207fea6076ec8cfe069599f515fee4408df265a7
JoMingyu <city7310@naver.com>
2018-05-02 10:53:29 +0900

    Flask New - 01

---
+++ 000. Flask/New/01. Hello World Server/main.py
@@ -0,0 +1,33 @@
+# https://github.com/pallets/flask
+# pip install flask
+from flask import Flask
+# 간단한 작업은 간단하게 할 수 있고, 큰 서버도 어렵지 않게 다룰 수 있다 = Flask는 핵심기능만 간결하게 유지하고, 확장 가능한 것을 목적으로 한다
+
+app = Flask(__name__)
+# 1. 현재 모듈에 대해 Flask 객체를 생성
+
+@app.route('/')
+# 2. 데코레이터를 통해 '/'에 라우팅
+# [app.Flask]
+#     def route(self, rule, **options) - 'A decorator that is used to register a view function for a given URL rule'
+
+# View Function - 한 URL Rule에 대해 할당된 함수
+# URL rule - Endpoint에서 HTTP 메소드를 제외한 부분('GET /'에서 'GET'을 뺀 '/' : URL)
+# 별도의 HTTP 메소드 명시가 없으면 GET에 라우팅하며, 따라서 아래의 view function은 'GET /'의 요청 처리를 담당
+def index():
+    return 'Hello World'
+    # 요청이 들어오면, 문자열 'Hello World'를 응답으로 전달. status code는 별도로 명시하지 않으면 '200 OK'로 처리
+
+
+@app.route('/custom/<int:param>')
+# Path parameter 정의 - <type:name>, 타입은 생략할 수 있으며, 생략한다면 타입은 str로 처리
+# 아래의 view function은 'GET /custom/{param}'에 대한 요청 처리를 담당
+def custom(param):
+    # Path parameter가 존재하면, view function에서 동일한 이름으로 인자를 받아 주어야 함
+    return param
+
+if __name__ == '__main__':
+    app.run()
+    # 3. 서버 run
+    # def run(self, host=None, port=None, debug=None, load_dotenv=True, **options)
+    # 기본적으로 host='127.0.0.1'(localhost), port=5000에서 debug=False 상태로 동작한다
