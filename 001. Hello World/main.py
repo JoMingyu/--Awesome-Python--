7be9e943af723b8f15f529312f31d1d82625811d
JoMingyu <city7310@naver.com>
2018-07-28 13:20:49 +0900

    001

--- 001. Hello World/main.py
+++ 001. Hello World/main.py
@@ -3,3 +3,6 @@ print('Hello World')
 # 주석은 '#' 기호로 쓴다
 print("Hello World")
 # 파이썬은 문자열을 감싸는 따옴표의 종류에 제한을 두지 않는다
+
+print('Hello'); print('Hi')
+# 파이썬도 세미콜론이 optional하지만, 단지 여러 statement를 한 줄에 이어 쓰기 위한 용도로만 사용할 수 있다
