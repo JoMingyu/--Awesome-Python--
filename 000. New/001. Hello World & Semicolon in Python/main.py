cb5e255578c9ca8db17dbcbab6ccbbc4f545c19a
JoMingyu <city7310@naver.com>
2018-09-02 13:45:57 +0900

    New - 001

---
+++ 000. New/001. Hello World & Semicolon in Python/main.py
@@ -0,0 +1,7 @@
+print('Hello World')
+# 파이썬의 확장자는 .py, 인터프리터 특성 상 한줄 한줄씩 읽어간다
+# 파이썬에는 이처럼 global namespace에 함수들이 여럿 지원된다
+
+print('Hello'); print('Hi')
+# 파이썬에서 세미콜론은 단지 여러 statement를 한 줄에 이어 쓰기 위한 용도로만 사용할 수 있다
+# 따라서 JavaScript 등에서 이야기하는 optional과는 조금 다르다
