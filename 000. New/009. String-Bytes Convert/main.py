266f1d1d469696087b69e12f2b182be55c98843c
JoMingyu <city7310@naver.com>
2018-09-04 21:32:58 +0900

    New - 009

---
+++ 000. New/009. String-Bytes Convert/main.py
@@ -0,0 +1,9 @@
+# 문자열과 bytes 간의 변환은, 문자열 객체의 encode()와 bytes 객체의 decode()를 사용한다
+'hello'.encode()
+# b'hello'
+b'hello'.decode()
+# 'hello'
+
+# 기본 인코딩은 utf-8로, ascii, utf-16 등 다른 종류의 인코딩을 사용할 수 있다
+'hello'.encode('utf-16')
+# b'\xff\xfeh\x00e\x00l\x00l\x00o\x00'
