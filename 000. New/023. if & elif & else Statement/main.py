3ed07c4797ec1182ca2e638991b13cc70d125c25
JoMingyu <city7310@naver.com>
2018-09-10 00:00:34 +0900

    New - 023

---
+++ 000. New/023. if & elif & else Statement/main.py
@@ -0,0 +1,16 @@
+# if statemant는 Python에서 조건문을 작성하기 위한 구문으로
+# 조건을 판단하여 작업을 분기시키는 데에 쓰인다
+# if [condition] 형태로 작성한다. condition 별도의 감싸는 괄호를 씌우지 않는다
+
+if True:
+    # Python은 block을 나타내기 위해, 대부분의 언어들에서 사용하는 중괄호 대신 콜론(:)과 들여쓰기를 사용
+    print('Condition is True!')
+
+if []:
+    print('[] is True!')
+elif ():
+    print('() is True!')
+elif {}:
+    print('{} is True!')
+else:
+    print('All is False')
