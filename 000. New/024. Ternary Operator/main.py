39ee5c70697eb5d31c6d9655be82ae44a311058a
JoMingyu <city7310@naver.com>
2018-09-10 00:00:38 +0900

    New - 024

---
+++ 000. New/024. Ternary Operator/main.py
@@ -0,0 +1,6 @@
+# Python의 삼항 연산자는 다른 언어들이 사용하는 [condition] ? [expr_when_true] : [expr_when_false] 형태가 아닌
+# if문을 사용하는 문법을 가지고 있다
+
+print('condition is true' if '' else 'condition is false') # 'condition is true'
+# [expr_when_true] if [condition] else [expr_when_false]
+# 삼항 연산자는 Python 2.5 이상 버전부터 지원된다
