4ebf62c3645f083548d8eb14f6076eaa1e8cd44c
JoMingyu <city7310@naver.com>
2018-09-09 12:01:00 +0900

    New - 021

---
+++ 000. New/021. Relational Operator & Relational Operator Chaining/main.py
@@ -0,0 +1,14 @@
+# 비교 연산자는 두 개의 표현식(expression)을 비교하여 참과 거짓을 판단하고,
+# boolean의 결과값을 얻기 위해 사용
+
+print(1 < 10) # True
+print(1 <= 10) # True
+
+print(10 > 1) # True
+print(10 >= 1) # True
+
+print(1 == 1) # True
+print(1 != 10) # True
+
+# Python의 비교 연산자는 이어붙이기(chaining) 가능하다
+print(1 < 10 < 20) # True
