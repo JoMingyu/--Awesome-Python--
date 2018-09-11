e59f7f2e7d22aeb672e9fe8f793a3b00f416656e
JoMingyu <city7310@naver.com>
2018-09-11 20:51:07 +0900

    New - 025

---
+++ 000. New/025. While Loop/main.py
@@ -0,0 +1,9 @@
+# while statement는 특정 조건이 False가 될 때까지 계속해서 코드를 반복하는 구문
+
+count = 0
+while count < 10:
+    # while [condition]
+    print('Current count : {}'.format(count))
+    count += 1
+
+# Python에는 do-while이 존재하지 않는다
